0dfac83e36fe2f0d28c2132a55de35aee11c7279
SunilaSooch <sunila.sooch@gmail.com>
2024-12-26 12:35:15 -0800

    more concise

--- life.py
+++ life.py
@@ -18,23 +18,16 @@ class ConwaysLife:
         self.size = size
         self.grid = np.zeros((size, size))
         self.timeflow = False
-        self.setup_plot()
         
-    def setup_plot(self):
         self.fig, self.ax = plt.subplots(figsize=(8, 8))
         self.img = self.ax.imshow(self.grid, cmap='binary', vmin=0, vmax=1)
         self.fig.canvas.mpl_connect('button_press_event', self.grid_select)
-        
-        # Add play/pause button
-        button_ax = self.fig.add_axes([0.0, 0.0, 0.2, 0.075])
-        self.button = Button(button_ax, 'Play/Pause')
+        self.button = Button(self.fig.add_axes([0, 0, 0.2, 0.075]), 'Play/Pause')
         self.button.on_clicked(lambda _: self.toggle_timeflow())
         
     def grid_select(self, event):
-        if event.inaxes != self.ax:
-            return
-        x, y = map(round, (event.xdata, event.ydata))
-        if 0 <= x < self.size and 0 <= y < self.size:
+        if event.inaxes == self.ax:
+            x, y = map(round, (event.xdata, event.ydata))
             self.grid[y, x] = 1 - self.grid[y, x]  
             self.img.set_array(self.grid)
             self.fig.canvas.draw()
@@ -43,12 +36,10 @@ class ConwaysLife:
         self.timeflow = not self.timeflow
         
     def tick(self, frame=None):
-        if not self.timeflow:
-            return [self.img]
-            
-        neighbors = convolve2d(self.grid, NEIGHBORS, mode='same', boundary='wrap')
-        self.grid = (((neighbors == 3) | ((self.grid == 1) & (neighbors == 2)))).astype(int)
-        self.img.set_array(self.grid)
+        if self.timeflow:
+            neighbors = convolve2d(self.grid, NEIGHBORS, mode='same', boundary='wrap')
+            self.grid = (((neighbors == 3) | ((self.grid == 1) & (neighbors == 2)))).astype(int)
+            self.img.set_array(self.grid)
         return [self.img]
         
     def run(self):
@@ -56,5 +47,4 @@ class ConwaysLife:
         plt.show()
 
 if __name__ == '__main__':
-    game = ConwaysLife()
-    game.run()
+    ConwaysLife().run()
